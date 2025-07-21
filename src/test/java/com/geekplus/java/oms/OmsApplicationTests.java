dd32f31639861e12de3a41ee864b3fb61c95986b
carlshenUS172 <carl.shen@geekplus.com>
2025-07-21 11:47:03 -0400

    initial commit

---
+++ src/test/java/com/geekplus/java/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekplus.java.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
